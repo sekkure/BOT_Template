d28c5c74fe39d4a19e3de56a165dd4b44eca109b
sekkure <secreori@gmail.com>
2022-06-26 07:46:20 +0300

    feat(scripts): script for registreting slash commands.

--- src/scripts/register-commands.ts
+++ src/scripts/register-commands.ts
@@ -1,20 +1,52 @@
 import { REST } from '@discordjs/rest'
+import { Command } from 'abstractions/Command'
 import { Routes } from 'discord-api-types/v9'
 import { config } from 'dotenv'
+import { Yamaoka } from 'index'
 import { join } from 'path'
+import { customReaddir } from 'utils/helpers'
 
 config({ path: join(process.cwd(), '.env') })
 
-const clientId = process.env.CLIENT_ID
-const guildId = process.env.GUILD_ID
-const token = process.env.TOKEN
+export const __init__ = async () => {
+  const clientId = process.env.CLIENT_ID
+  const guildId = process.env.GUILD_ID
+  const token = process.env.TOKEN
 
-if (!token || !clientId || !guildId) {
-  throw new Error('You must provide valid data in .env file!')
+  if (!token || !clientId || !guildId) {
+    throw new Error('You must provide valid data in .env file!')
+  }
+
+  console.log('Slash commands deploy started, collecting all commands...')
+
+  const commands = []
+  const path = join(__dirname, '..', 'commands', '**', '*')
+  const files = await customReaddir(path, ['js', 'ts'])
+
+  for (const file of files) {
+    const command: Command = new (await import(file)).default(Yamaoka)
+    const proto = Object.getPrototypeOf(command)
+
+    if (!proto.isSlashCommand) continue
+
+    commands.push(command.data)
+  }
+
+  console.log(
+    `Collected total ${files.length} commands, slash commands ${commands.length}`
+  )
+
+  if (!commands.length) {
+    return console.log('0 slash commands was found, aborting')
+  }
+
+  new REST()
+    .setToken(token)
+    .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => {
+      console.log(
+        `Successfully registered total of ${commands.length} slash commands!`
+      )
+    })
+    .catch(console.error)
 }
-
-new REST({ version: '9' })
-  .setToken(token)
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
-  .then(() => console.log('Successfully registered ( / ) commands!'))
-  .catch(console.error)
