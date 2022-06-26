92d933e7d8789ae66aae951245722d9ee69d0d02
sekkure <secreori@gmail.com>
2022-06-26 07:44:53 +0300

    feat(index.ts): Base setup to start bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export {}
+import { config } from 'dotenv'
+import { join } from 'path'
+import { YamaokaClient } from 'structure/YamaokaClient'
+
+config({ path: join(process.cwd(), '.env') })
+
+export const Yamaoka = new YamaokaClient({
+  intents: [],
+})
+
+const main = async () => {
+  await Yamaoka.init()
+  await Yamaoka.login(process.env.TOKEN)
+}
+
+main()
