715d83ea2cf49d0662df1c89edd91a717739abc1
sekkure <secreori@gmail.com>
2022-06-26 07:47:09 +0300

    feat(base): simple client structure with two must have methods

---
+++ src/structure/YamaokaClient.ts
@@ -0,0 +1,55 @@
+import { Command } from 'abstractions/Command'
+import { Event } from 'abstractions/Event'
+import { Client, ClientOptions, Collection } from 'discord.js'
+import { join } from 'path'
+import { customReaddir } from 'utils/helpers'
+
+export class YamaokaClient extends Client {
+  public commands: Collection<string, Command> = new Collection()
+
+  constructor(options: ClientOptions) {
+    super(options)
+  }
+
+  public async init() {
+    await this.collectCommand()
+    await this.collectEvents()
+  }
+
+  private async collectEvents() {
+    try {
+      console.log('Starting loading some events...')
+
+      const path = join(__dirname, '..', 'events', '**', '*')
+      const files = await customReaddir(path, ['js', 'ts'])
+
+      for (const file of files) {
+        const event = new (await import(file)).default()
+
+        this.on(event.name, event.execute)
+      }
+
+      console.log('Events successfuly loaded!')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  private async collectCommand() {
+    try {
+      console.log(`Starting loading some commands to cache...`)
+      const path = join(__dirname, '..', 'commands', '**', '*')
+      const files = await customReaddir(path, ['js', 'ts'])
+
+      for (const file of files) {
+        const command: Command = new (await import(file)).default(this)
+
+        this.commands.set(command.data.name, command)
+      }
+
+      console.log(`${this.commands.size} commands was loaded to local cache!`)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
